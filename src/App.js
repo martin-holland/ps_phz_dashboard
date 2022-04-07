a5fd9ecd3347beae1f90f9c5b6c767b4f50fdac3
meanjula <anjula.ghale@gmail.com>
2022-04-08 00:12:41 +0300

    added firebase config and retrive data from firestore

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ import { useState, useEffect } from "react";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
+  const [firebaseData, setFirebaseData] = useState([]);
+  useEffect(() => {
+    const docRef = collection(db, "survey_results");
+    getDocs(docRef)
+      .then((snapshot) => {
+        // console.log(snapshot.docs);
+        if (snapshot.empty) {
+          console.log("No data available");
+        } else {
+          setFirebaseData(
+            snapshot.docs.map((doc) => ({ id: doc.id, result: doc.data() }))
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(firebaseData.map((data) => data.result));
   return (
     <div className="App">
       <Provider store={store}>
@@ -17,6 +36,7 @@ function App() {
       </Provider>
       <h1>dashboard</h1>
       <Dashboard />
+      <h1>dashboard</h1>
     </div>
   );
 }
