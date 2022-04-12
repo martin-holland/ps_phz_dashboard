9c0defff21c165f1214a96b89d8a1b907649a62b
meanjula <anjula.ghale@gmail.com>
2022-04-12 12:09:08 +0300

    initialize the components for dashboard

--- src/App.js
+++ src/App.js
@@ -10,25 +10,6 @@ import { useState, useEffect } from "react";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
-  const [firebaseData, setFirebaseData] = useState([]);
-  useEffect(() => {
-    const docRef = collection(db, "survey_results");
-    getDocs(docRef)
-      .then((snapshot) => {
-        // console.log(snapshot.docs);
-        if (snapshot.empty) {
-          console.log("No data available");
-        } else {
-          setFirebaseData(
-            snapshot.docs.map((doc) => ({ id: doc.id, result: doc.data() }))
-          );
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  console.log(firebaseData.map((data) => data.result));
   return (
     <div className="App">
       <Provider store={store}>
@@ -37,6 +18,7 @@ function App() {
       <h1>dashboard</h1>
       <Dashboard />
       <h1>dashboard</h1>
+      <Dashboard />
     </div>
   );
 }
