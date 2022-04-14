fcd758463344f51164caa9c35aaf1751903a39d7
meanjula <anjula.ghale@gmail.com>
2022-04-14 20:32:47 +0300

    fixed the styling of message box

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const getAllData = () => (dispatch) => {
   dispatch({ type: LOADING_DATA });
   axios
-    .get("/user")
+    .get("https://us-central1-promoterscore-14480.cloudfunctions.net/api/data")
     .then((res) => {
       dispatch({
         type: SET_DATA,
