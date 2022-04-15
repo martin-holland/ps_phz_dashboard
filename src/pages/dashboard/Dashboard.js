4361a2c2047ca3752e769a9eea8d6acb2a4f06ce
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-04-15 12:39:19 +0300

    dashboard data retrieval fixed

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -5,6 +5,12 @@ import "./Dashboard.css";
 import BarChart from "../../components/chart/BarChart";
 import Message from "../../components/message/Message";
 
+// Redux imports
+import { connect } from "react-redux";
+import { getAllData } from "../../redux/actions/dataActions";
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+
 const Main = styled.div`
   background-color: ${(props) => props.theme.backgroundColor};
   transition: all 0.5s ease;
@@ -53,7 +59,8 @@ const MessageContainer = styled.div`
 `;
 const Dashboard = (props) => {
   // Data destructuring
-  const { results, loading } = props;
+  const { results, loading } = props.data;
+  console.log("results from dashboard: ", results);
 
   //theme
   const changeTheme = () => {
@@ -61,36 +68,51 @@ const Dashboard = (props) => {
   };
   const icon = props.theme === "light" ? <BsMoonStarsFill /> : <BsLightbulb />;
 
-  // if (!loading && results !== undefined && results.length >= 1) {
-  //   console.log("results from dashboard: ", results);
-  return (
-    <Main className="main">
-      <div className="container">
-        <div className="header-container">
-          <TitleHead className="header">
-            Net Promoter Score Calculation
-          </TitleHead>
-          <Toggle className="toggle-button" onClick={changeTheme}>
-            {icon}
-          </Toggle>
+  // Data retrieval
+  useEffect(() => {
+    props.getAllData();
+    //eslint-disable-next-line
+  }, []);
+
+  if (!loading && results !== undefined && results.length >= 1) {
+    console.log("results from dashboard: ", results);
+    return (
+      <Main className="main">
+        <div className="container">
+          <div className="header-container">
+            <TitleHead className="header">
+              Net Promoter Score Calculation
+            </TitleHead>
+            <Toggle className="toggle-button" onClick={changeTheme}>
+              {icon}
+            </Toggle>
+          </div>
+          <SubTitie>
+            <p>Net Promoter Score calculations with breakouts and deltas.</p>
+          </SubTitie>
+          <ChartContainer className="chart-container">
+            <BarChart theme={props.theme} />
+          </ChartContainer>
+          <MessageContainer className="message-container">
+            {results?.slice(0, 5).map((result) => (
+              <Message result={result} key={result.id} />
+            ))}
+          </MessageContainer>
         </div>
-        <SubTitie>
-          <p>Net Promoter Score calculations with breakouts and deltas.</p>
-        </SubTitie>
-        <ChartContainer className="chart-container">
-          <BarChart theme={props.theme} />
-        </ChartContainer>
-        <MessageContainer className="message-container">
-          {results?.slice(0, 5).map((result) => (
-            <Message result={result} key={result.id} />
-          ))}
-        </MessageContainer>
-      </div>
-    </Main>
-  );
-  // } else {
-  //   return <div>Data Loading...</div>;
-  // }
+      </Main>
+    );
+  } else {
+    return <div>Data Loading...</div>;
+  }
 };
 
-export default Dashboard;
+Dashboard.propTypes = {
+  getAllData: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data,
+});
+
+export default connect(mapStateToProps, { getAllData })(Dashboard);
