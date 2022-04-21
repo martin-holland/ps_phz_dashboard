24c0068bf5e5b3a91cfa115634b1384e5c77d033
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-04-21 12:27:57 +0300

    fixed conditional rendering of signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,6 @@
 import "./Signup.css";
 import React, { Component } from "react";
+import Dashboard from "./dashboard/Dashboard";
 
 // Redux Imports
 import PropTypes from "prop-types";
@@ -50,61 +51,67 @@ export class Signup extends Component {
     } = this.props;
     const { errors } = this.state;
 
-    return (
-      <div id="login">
-        <div id="welcome"><strong>Let's get started!</strong></div>
-        <form noValidate onSubmit={this.handleSubmit}>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            label="Email"
-            placeholder="Enter email address"
-            value={this.state.email}
-            onChange={this.handleChange}
-          ></input>
-          <p>{errors.email}</p>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            label="Password"
-            placeholder="Enter password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          ></input>
-          <p>{errors.password}</p>
-          <input
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            label="Confirm Password"
-            placeholder="Confirm password"
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
-          ></input>
-          <p>{errors.confirmPassword}</p>
-          <input
-            id="handle"
-            name="handle"
-            type="text"
-            label="Username"
-            placeholder="Enter a username"
-            value={this.state.handle}
-            onChange={this.handleChange}
-          ></input>
-          <p>{errors.handle}</p>
-          <Link to="/">
-            <button id="signup" type="submit">
+    if (this.props.user.authenticated) {
+      return (
+        <Dashboard theme={this.props.theme} setTheme={this.props.setTheme} />
+      );
+    } else {
+      return (
+        <div id="login">
+          <div id="welcome">
+            <strong>Let's get started!</strong>
+          </div>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="Enter email address"
+              value={this.state.email}
+              onChange={this.handleChange}
+            ></input>
+            <p>{errors.email}</p>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              placeholder="Enter password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            ></input>
+            <p>{errors.password}</p>
+            <input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              placeholder="Confirm password"
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+            ></input>
+            <p>{errors.confirmPassword}</p>
+            <input
+              id="handle"
+              name="handle"
+              type="text"
+              label="Username"
+              placeholder="Enter a username"
+              value={this.state.handle}
+              onChange={this.handleChange}
+            ></input>
+            <p>{errors.handle}</p>
+            <button id="signup" type="submit" onClick={this.handleSubmit}>
               Register
             </button>
-          </Link>
-          <Link to="/">
-            <div id="account">Already have an account?</div>
-          </Link>
-        </form>
-      </div>
-    );
+            <Link to="/">
+              <div id="account">Already have an account?</div>
+            </Link>
+          </form>
+        </div>
+      );
+    }
   }
 }
 
