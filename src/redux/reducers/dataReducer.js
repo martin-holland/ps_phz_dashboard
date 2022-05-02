923c1d717c2dfb5712a505ac973f573512a850df
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-05-03 02:17:41 +0300

    dates by year and month in state

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,8 +1,18 @@
-import { LOADING_DATA, SET_DATA, SET_NEW_RESULTS } from "../types";
+import {
+  DATE_ORDERED,
+  LOADING_DATA,
+  SET_DATA,
+  SET_NEW_RESULTS,
+  MONTH_ORDERED,
+  YEARS_AND_MONTHS_ORDERED,
+} from "../types";
 
 const initialState = {
   results: [],
   defaultResults: [],
+  datesByYear: [],
+  datesByMonth: [],
+  datesByYearAndMonth: [],
   loading: false,
 };
 
@@ -24,6 +34,24 @@ export default function (state = initialState, action) {
       return {
         ...state,
         defaultResults: action.payload,
+        loading: false,
+      };
+    case DATE_ORDERED:
+      return {
+        ...state,
+        datesByYear: action.payload,
+        loading: false,
+      };
+    case MONTH_ORDERED:
+      return {
+        ...state,
+        datesByMonth: action.payload,
+        loading: false,
+      };
+    case YEARS_AND_MONTHS_ORDERED:
+      return {
+        ...state,
+        datesByYearAndMonth: action.payload,
         loading: false,
       };
     default:
